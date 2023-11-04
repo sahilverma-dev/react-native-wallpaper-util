2af055f64465b17c87f9ddb74bbd3e734dc1c6b5
sahilverma-dev <domainsahil007@gmail.com>
2023-11-04 19:45:27 +0530

    minor fix

--- index.d.ts
+++ index.d.ts
@@ -2,14 +2,12 @@ declare module "react-native-wallpaper-util" {
   interface ImageURISource {
     uri: string;
     screen: "HOME" | "LOCK" | "BOTH";
+    callback?: SetWallpaperCallback;
   }
 
   type SetWallpaperCallback = () => void;
 
-  const setWallpaper: (
-    source: ImageURISource,
-    callback: SetWallpaperCallback
-  ) => void;
+  const setWallpaper: (source: ImageURISource) => void;
 
   export { setWallpaper };
 }
